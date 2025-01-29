e1b36c97262c84d6fb7a389dad650d197712b0aa
NicolasEstebanCuervo <rojascuervo942@gmail.com>
2025-01-28 21:52:12 -0500

    Path in App.ts fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import Transform from "../sections/transform";
-import Footer from "../sections/footer";
-import Contact from "../sections/contact";
-import Seed from "../sections/seed";
-import Header from "../sections/header";
-import CreativeAI from "../sections/creativeAI";
+import Footer from "./sections/footer";
+import Contact from "./sections/contact";
+import Seed from "./sections/seed";
+import Header from "./sections/header";
+import CreativeAI from "./sections/creativeAI";
+import Transform from "./sections/transform";
 
 function App() {
     return (
